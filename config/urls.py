f844424823d46d5c823b0a82f1f96a1e5e32795a
Will17hl <andreshl.910@gmail.com>
2026-05-12 08:43:29 -0500

    feat: Fix media handling in production

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic import RedirectView
+from django.views.static import serve
 
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
@@ -29,6 +30,12 @@ urlpatterns = [
 
 # Serve media files in production
 if settings.MEDIA_URL and settings.MEDIA_ROOT:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    if settings.DEBUG:
+        urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    else:
+        media_path = settings.MEDIA_URL.lstrip("/")
+        urlpatterns += [
+            re_path(rf'^{media_path}(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+        ]
 
 urlpatterns += staticfiles_urlpatterns()
